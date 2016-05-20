e4c703a17f829e3d64df6e8711596af57b037ac8
jacqueline-homan <jacquelinehoman7@gmail.com>
2016-05-20 19:52:42 -0400

    first and final commit

---
+++ AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module IObasics1.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("IObasics1")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("Codecatenation, LLC")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
---
+++ Program.fs
@@ -0,0 +1,19 @@
+﻿let x = 42
+let hi = "Hello"
+let sayHiTo me =
+    printfn "Hi, %s" me
+sayHiTo "J"
+
+let square x = x * x
+printfn "Enter an integer: "
+let response = System.Console.ReadLine() |> int
+
+printfn "%A" (square x, response)
+
+
+
+[<EntryPoint>]
+let main argv = 
+    //printfn "%A" argv
+    0 // return an integer exit code
+
